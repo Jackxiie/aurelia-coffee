ed7b7426800ef1228b27d58ac1d6b39fe128ddf5
Jackxiie <xaypanyavxl@gmail.com>
2026-03-27 22:30:44 +0700

    remove status filter from product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,39 +7,36 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function home()
-{
-    $products = Product::where('type', 'coffee')
-        ->take(8)
-        ->get();
+    {
+        $products = Product::where('type', 'coffee')
+            ->take(8)
+            ->get();
 
-    return view('home', compact('products'));
-}
+        return view('home', compact('products'));
+    }
 
-public function menu()
-{
-    $desserts = Product::where('type', 'dessert')->get();
-    $drinks = Product::where('type', 'drink')->get();
-    $starters = Product::where('type', 'starter')->get();
-    $mainDishes = Product::where('type', 'main dish')->get();
-    $coffees = Product::where('type', 'coffee')->get();
+    public function menu()
+    {
+        $desserts = Product::where('type', 'dessert')->get();
+        $drinks = Product::where('type', 'drink')->get();
+        $starters = Product::where('type', 'starter')->get();
+        $mainDishes = Product::where('type', 'main dish')->get();
+        $coffees = Product::where('type', 'coffee')->get();
 
-    return view('menu', compact(
-        'desserts',
-        'drinks',
-        'starters',
-        'mainDishes',
-        'coffees'
-    ));
-}
+        return view('menu', compact(
+            'desserts',
+            'drinks',
+            'starters',
+            'mainDishes',
+            'coffees'
+        ));
+    }
 
     public function show($id)
     {
-        $product = Product::with('sizes')
-            ->where('status', 'active')
-            ->findOrFail($id);
+        $product = Product::with('sizes')->findOrFail($id);
 
         $relatedProducts = Product::where('type', $product->type)
-            ->where('status', 'active')
             ->where('id', '!=', $product->id)
             ->take(4)
             ->get();
